012ccb757e8543337f2039ea1c13ed1c803363b1
Julien Omedes <julien.omedes@outlook.fr>
2025-10-09 11:43:06 +0200

    front_new_organization_add_first_components_with_lazy_loading

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: 'register', loadChildren: () => import('./pages/components/register/register.module').then(m => m.RegisterModule) },
+  { path: 'login', loadChildren: () => import('./pages/components/login/login.module').then(m => m.LoginModule) },
+  { path: 'posts/:id', loadChildren: () => import('./pages/components/post/post.module').then(m => m.PostModule) },
+  { path: 'posts', loadChildren: () => import('./pages/components/posts/posts.module').then(m => m.PostsModule) },
+  { path: '', loadChildren: () => import('./pages/components/home/home.module').then(m => m.HomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
